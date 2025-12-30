14728dc189bc0b1be3157845323af6908f33e6bc
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2025-12-30 19:37:02 +0300

    add AuthForm.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App/App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate >
+    <PersistGate>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>
-)
+);

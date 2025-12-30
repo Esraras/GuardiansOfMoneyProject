bdb8dbc2ba857ca20371a8d914468571fc64188b
hilal_dinc <hilaldincc94@gmail.com>
2025-12-30 11:12:10 +0300

    feat: add currency slice to Redux store

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import authReducer from "../features/auth/authSlice";
+import currencyReducer from "../features/currency/currencySlice";
+
+// Persist sadece auth için
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+  currency: currencyReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefault) =>
+    getDefault({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);

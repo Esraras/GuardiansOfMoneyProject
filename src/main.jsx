77dc3b5ae2bf8ed520918b6d23f58d9ef1143e35
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2025-12-31 11:16:21 +0300

    login-register-authform

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App/App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+import "modern-normalize";
+import "./index.css";
+
+import App from "./App";
+
+import { persistor, store } from "./redux/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <ToastContainer theme="dark" />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );

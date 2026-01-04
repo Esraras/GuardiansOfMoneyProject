4a831a97be80d16fe56d9d5a269b1268bfaa3e1e
esraras <eesraras29@gmail.com>
2026-01-05 01:18:32 +0300

    Remove base for Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Base path for GitHub Pages (repo pages). Update to your repository name if different.
-  base: "/GuardiansOfMoneyProject/",
   plugins: [react()],
   build: {
     sourcemap: true,

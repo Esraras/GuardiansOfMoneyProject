ec6cf1d51f267e3c06d75a3634c2e376e4ad2d18
esraras <eesraras29@gmail.com>
2026-01-05 00:46:09 +0300

    düzenleme

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -14,13 +15,15 @@ import App from "./components/App/App";
 
 import { persistor, store } from "./redux/store";
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-        <ToastContainer theme="dark" />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <ToastContainer theme="dark" />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
